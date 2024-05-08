6b210f64e31fff7f026d790acaf69ac9493fbe39
andreamoglianesi <andreamoglianesi@hotmail.it>
2024-05-08 12:21:38 +0200

    Update to the format of the final_v2_df file

--- static/data_preprocessing.py
+++ static/data_preprocessing.py
@@ -19,14 +19,16 @@ products_df = pd.read_csv(StringIO(products_csv))
 # Create a new DataFrame for final output
 final_df = products_df[['Product Name', 'product_code', 'Type']].copy()
 
+# Rename columns
+final_df.rename(columns={'Product Name': 'product_name', 'Type': 'type'}, inplace=True)
+
 # Initialize columns for each year
 for year in range(2019, 2024):
-    final_df[f'Revenue - {year}'] = 0
-    final_df[f'Quantity - {year}'] = 0
+    final_df[f'revenue_{year}'] = 0
+    final_df[f'quantity_{year}'] = 0
 
 # Process orders and update final_df
 for index, row in orders_df.iterrows():
-    #products_names = row["Products"].split(', ')
     products_id = map(int, row["productsIDs"].split(', '))
     quantities = map(int, row["Quantities"].split(', '))
     prices = map(float, row["ProductPricesInCP"].split(', '))
@@ -34,28 +36,23 @@ for index, row in orders_df.iterrows():
     order_date = pd.to_datetime(row["OrderDate"]).year
 
     for product_ids, quantity, price in zip(products_id, quantities, prices):
-        # Find the index of the row in final_df where 'Product Name' matches product_name
-        #index = final_df.index[final_df['Product Name'] == product_name].tolist()[0]
-        # Find the index of the row in final_df where product_code matches product_ids
         index = final_df.index[final_df['product_code'] == product_ids].tolist()[0]
-        # Update the quantity for the corresponding product ID by adding the new quantity
-        final_df.at[index, f'Quantity - {order_date}'] += quantity
-        # Update the price for the corresponding product ID by adding the new price
-        final_df.at[index, f'Revenue - {order_date}'] = final_df.at[index, f'Revenue - {order_date}'] + price
+        final_df.at[index, f'quantity_{order_date}'] += quantity
+        final_df.at[index, f'revenue_{order_date}'] += price
 
 # Create final_v2_df
-final_v2_df = final_df.groupby('Product Name').agg({
-    'Type': 'first',  # Keep the original Product Type value
-    'Quantity - 2019': 'sum', 
-    'Quantity - 2020': 'sum', 
-    'Quantity - 2021': 'sum', 
-    'Quantity - 2022': 'sum', 
-    'Quantity - 2023': 'sum', 
-    'Revenue - 2019': 'sum', 
-    'Revenue - 2020': 'sum', 
-    'Revenue - 2021': 'sum', 
-    'Revenue - 2022': 'sum', 
-    'Revenue - 2023': 'sum'
+final_v2_df = final_df.groupby('product_name').agg({
+    'type': 'first',  # Keep the original Product Type value
+    'quantity_2019': 'sum', 
+    'quantity_2020': 'sum', 
+    'quantity_2021': 'sum', 
+    'quantity_2022': 'sum', 
+    'quantity_2023': 'sum', 
+    'revenue_2019': 'sum', 
+    'revenue_2020': 'sum', 
+    'revenue_2021': 'sum', 
+    'revenue_2022': 'sum', 
+    'revenue_2023': 'sum'
 }).reset_index()
 
 # Save final_df and final_v2_df to CSV files
