2cdf0e0d0beb768bd32da595adadd096ac88e2d4
andreamoglianesi <andreamoglianesi@hotmail.it>
2024-05-14 14:55:31 +0200

    Updates to popular_products_svelte

--- src/routes/popular_products_svelte/+page.svelte
+++ src/routes/popular_products_svelte/+page.svelte
@@ -2,6 +2,9 @@
 	import * as d3 from 'd3';
 	import {scaleOrdinal} from 'd3-scale';
 	import {schemeCategory10} from 'd3-scale-chromatic';
+    import { onMount } from 'svelte';
+    import { writable } from 'svelte/store';
+    import { axisBottom, axisLeft } from 'd3-axis';
 
 	function filterAndSelectProducts(typeselection, products, year, n) {
         // Filter products based on typeselection
@@ -85,6 +88,9 @@
         return rescale(bp.revenue, total, inner_height);
     };
 
+    // Create a color scale
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
 	// Function to assign color based on product_name
 	const getColor = (product_name) => {
 		// If the product_name is 'Other', return grey
@@ -92,75 +98,110 @@
 			return 'grey'; // You can replace 'grey' with any other color code
 		}
 		// Otherwise, use the color scale
-		const colorScale = d3.scaleOrdinal(d3.schemePaired);
 		return colorScale(product_name);
 	};
-	
- 	// Loop through each year and create stacked bars
- 	let stackedBarsHTML = '';
-    const years = [2019, 2020, 2021, 2022, 2023];
-    years.forEach(year => {
-        // Filter and select products for the current year
-        const bestProducts = filterAndSelectProducts(typeselection, data, year, n);
-		console.log(bestProducts)
 
-        // Calculate total revenue for the current year
-        let yeartotal = bestProducts.reduce((acc, product) => acc + product.revenue, 0);
+    function handleSliderInput(event) {
+        n = parseInt(event.target.value); // Update the value of n
+        updateVisualization(); // Update the visualization with the new value of n
+    }
 
-        // Compare with maxTotal and update if necessary
-        if (yeartotal > total) {
-            total = yeartotal;
-        }
+    // Function to handle dropdown selection
+    function handleDropdownChange(event) {
+        typeselection = event.target.value; // Update the value of typeselection
+        updateVisualization(); // Update the visualization with the new value of typeselection
+    }
 
+    // Create a writable store to hold the HTML content
+    const stackedBarsHTMLStore = writable('');
+
+    function updateVisualization(){
+        let newstackedBarsHTML = ''; // Initialize a new variable to hold the HTML content
         // Construct SVG for the current year
         let svgHTML = `<svg width="${width}" height="${height}" class="translated" transform="translate(${margin.left}, ${margin.top})">`;
+        // Re-initialize the value of total
+        total = 0;
+        // Loop through each year and create stacked bars
+        const years = [2019, 2020, 2021, 2022, 2023];
+        years.forEach(year => {
+            // Filter and select products for the current year
+            const bestProducts = filterAndSelectProducts(typeselection, data, year, n);
+            console.log(bestProducts)
 
-        // Loop through each product for the current year
-        bestProducts.slice().reverse().forEach((bp, i) => {
-			// console.log(bestProducts[i].revenue)
-            // Calculate x position based on size of the bars
-            const x = calculateX(margin, year, bar_width, bar_distance);
-            console.log(i)
-			let y = 0;
-			// Calculate y position based on cumulativeHeight
-			if (i == 0){
-				y = height - rescale(bp.revenue, total, inner_height);
+            // Calculate total revenue for the current year
+            let yeartotal = bestProducts.reduce((acc, product) => acc + product.revenue, 0);
+
+            // Compare with maxTotal and update if necessary
+            if (yeartotal > total) {
+                total = yeartotal;
+            }
+            // Construct SVG for the current year
+            let svgHTML = `<svg width="${width}" height="${height}" class="translated" transform="translate(${margin.left}, ${margin.top})">`;
+
+            // Loop through each product for the current year
+            bestProducts.slice().reverse().forEach((bp, i) => {
+                // Calculate x position based on size of the bars
+                const x = calculateX(margin, year, bar_width, bar_distance);
+                let y = 0;
+                // Calculate y position based on cumulativeHeight
+                if (i == 0){
+                    y = height - rescale(bp.revenue, total, inner_height);
+                    //console.log(y)
+                }else{
+                    let cumulativeHeight = 0;
+                    const N = bestProducts.length;
+                    for (let j = N - 1 ; j >= N - i; j--) {
+                        const revenue = parseFloat(bestProducts[j].revenue);
+                        cumulativeHeight += revenue;
+                    }
+                y = height - rescale(cumulativeHeight + bp.revenue, total, inner_height);
                 //console.log(y)
-			}else{
-				let cumulativeHeight = 0;
-				const N = bestProducts.length;
-				for (let j = N - 1 ; j >= N - i; j--) {
-                    const revenue = parseFloat(bestProducts[j].revenue);
-					cumulativeHeight += revenue;
-					//console.log(j)
-					//console.log(bestProducts[j].revenue)
-					//console.log(cumulativeHeight)
-				}
-			y = height - rescale(cumulativeHeight + bp.revenue, total, inner_height);
-            //console.log(y)
-			}
-            console.log(y)
-            console.log(calculateHeight(bp))
+                }
 
-            // Construct rect element for the current product
-            const rectHTML = `<rect x="${x}" y="${y}" width="${bar_width}" height="${calculateHeight(bp)}" fill="${getColor(bp.product_name)}"></rect>`;
+                // Construct rect element for the current product
+                const rectHTML = `<rect x="${x}" y="${y}" width="${bar_width}" height="${calculateHeight(bp)}" fill="${getColor(bp.product_name)}"></rect>`;
 
-            // Append rect element to SVG HTML
-            svgHTML += rectHTML;
-        });
+                // Append rect element to SVG HTML
+                svgHTML += rectHTML;
+            });
 
         // Close SVG tag
         svgHTML += `</svg>`;
 
         // Add SVG HTML for the current year to stackedBarsHTML
-        stackedBarsHTML += svgHTML;
+        newstackedBarsHTML += svgHTML;
 
-    // Calculate x position based on size of the bars
-    const textX = width / 2; // Center the text horizontally
-    const textY = height + margin.top + margin.bottom + 20; // Adjust this value to position the text properly
-    stackedBarsHTML += `<text x="${textX}" y="${textY}" font-family="Arial" font-size="12" text-anchor="middle">${year}</text>`;
-    });
+        });
+
+
+        // Update the value of the store with the new HTML content
+        stackedBarsHTMLStore.set(newstackedBarsHTML);
+    }
+
+    onMount(updateVisualization);
 
 </script>
 
-{@html stackedBarsHTML}
+<div>
+    <label for="slider">Number of Top Elements (n):</label>
+    <input type="range" id="slider" min="3" max="20" step="1" bind:value={n} on:input={handleSliderInput}>
+    <span>{n}</span>
+</div>
+
+<div>
+    <label for="typeselection">Product Type Selection:</label>
+    <select id="typeselection" bind:value={typeselection} on:change={handleDropdownChange}>
+        <option value="ALL">All Products</option>
+        <option value="ADVENTURING EQUIPMENT">Adventuring Equipment</option>
+        <option value="ANIMALS & TRANSPORTATION">Animals & Transportation</option>
+        <option value="ARMS & ARMOUR">Arms & Armour</option>        
+        <option value="JEWELRY">Jewelry</option>
+        <option value="MUSICAL INSTRUMENT">Musical Instrument</option>
+        <option value="POTIONS & SCROLLS">Potions & Scrolls</option>
+        <option value="SUMMONING DEVICE">Summoning Device</option>
+        <option value="TOOLS & KITS">Tools & Kits</option>
+        <!-- Add more options as needed -->
+    </select>
+</div>
+
+{@html $stackedBarsHTMLStore}
