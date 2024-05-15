bb3a28b8e505d0847d598a53f5bdc4d7134f17c0
andreamoglianesi <andreamoglianesi@hotmail.it>
2024-05-15 09:56:57 +0200

    Final updates with interactions

--- src/routes/popular_products_svelte/+page.svelte
+++ src/routes/popular_products_svelte/+page.svelte
@@ -6,7 +6,9 @@
     import { writable } from 'svelte/store';
     import { axisBottom, axisLeft } from 'd3-axis';
 
-	function filterAndSelectProducts(typeselection, products, year, n) {
+    let includeOther = true;
+
+	function filterAndSelectProducts(typeselection, products, year, n, includeOther) {
         // Filter products based on typeselection
 
         let filteredProducts;
@@ -22,17 +24,28 @@
         // Select the first n products
         let bestProducts = sortedProducts.slice(0, n);
 
-		// Calculate total revenue for the given year
-		let total = filteredProducts.reduce((acc, product) => acc + (product[`revenue_${year}`] || 0), 0);
-
-        // Calculate total revenue and quantity for the remaining products
         let otherRevenue = 0;
-        let otherQuantity = 0;
-        for (let i = n; i < sortedProducts.length; i++) {
-            otherRevenue += sortedProducts[i][`revenue_${year}`];
-            otherQuantity += sortedProducts[i][`quantity_${year}`];
+        let otherQuantity = 0;        
+
+        if(includeOther){
+            // Calculate total revenue and quantity for the remaining products
+            for (let i = n; i < sortedProducts.length; i++) {
+                otherRevenue += sortedProducts[i][`revenue_${year}`];
+                otherQuantity += sortedProducts[i][`quantity_${year}`];
+            }        
+        }else{
+            otherRevenue = 0;
+            otherQuantity = 0;                    
         }
 
+        // Create 'Other' product
+        let otherProduct = {
+                product_name: 'Other',
+                type: 'Other',
+                [`revenue`]: otherRevenue,
+                [`quantity`]: otherQuantity
+        }
+        
         // Filter out unwanted columns
         bestProducts = bestProducts.map(product => ({
                 product_name: product.product_name,
@@ -41,16 +54,10 @@
                 [`quantity`]: product[`quantity_${year}`]
             }));
 
-        // Create 'Other' product
-        let otherProduct = {
-            product_name: 'Other',
-            type: 'Other',
-            [`revenue`]: otherRevenue,
-            [`quantity`]: otherQuantity
-        };
-
+        
         // Push 'Other' product to bestProducts
         bestProducts.push(otherProduct);
+        
 
         return bestProducts;
 	}
@@ -58,19 +65,19 @@
 	export let data;
 		
 	let n = 10; // Number of top n elements to show
-	let typeselection = 'TOOLS & KITS'; // Which product types to show
+	let typeselection = 'ALL'; // Which product types to show
 	let total = 0;
 	// To create visualisations we need to know how large the image will be.
-	const width = 100;
-	const height = 300;
+	const width = 1200;
+	const height = 900;
 	// We usually reserve some space for the axes by specifying margins.
-	const margin = { top: 10, right: 10, bottom: 50, left: 50 };
+	const margin = { top: 40, right: 20, bottom: 100, left: 80 };
 	// I call the remaining space the "content area". It tends to be useful
 	// to give that space's size names as well.
 	const inner_width = width - margin.left - margin.right;
 	const inner_height = height - margin.top - margin.bottom;
 	// Also, we need to know how large the bars are
-	const bar_width = 50;
+	const bar_width = 150;
 	// And how far they are from each other
 	const bar_distance = (inner_width - bar_width * 5)/4;
 		
@@ -80,7 +87,7 @@
 
 	// Function to calculate the x-coordinate based on the year and size of the bar
 	const calculateX = (margin, year, bar_width, bar_distance) => {
-		return margin.right + (year-2019) * (bar_width + bar_distance)
+		return margin.left + (year-2019) * (bar_width + bar_distance)
 	}
 
     // Function to calculate the height of each bar
@@ -88,14 +95,22 @@
         return rescale(bp.revenue, total, inner_height);
     };
 
+    // Create a list of custom colors
+    const customColors = [
+    "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf", 
+    "#aec7e8", "#ffbb78", "#98df8a", "#ff9896", "#c5b0d5", "#c49c94", "#f7b6d2", "#c7c7c7", "#dbdb8d", "#9edae5",
+    "#393b79", "#5254a3", "#6b6ecf", "#9c9ede", "#637939", "#8ca252", "#b5cf6b", "#cedb9c", "#8c6d31", "#bd9e39",
+    "#e7ba52", "#e7cb94", "#843c39", "#ad494a", "#d6616b", "#e7969c", "#7b4173", "#a55194", "#ce6dbd", "#de9ed6"
+    ];
+
     // Create a color scale
-    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+    const colorScale = d3.scaleOrdinal(customColors);
 
 	// Function to assign color based on product_name
 	const getColor = (product_name) => {
 		// If the product_name is 'Other', return grey
 		if (product_name === 'Other') {
-			return 'grey'; // You can replace 'grey' with any other color code
+			return 'lightgrey'; // You can replace 'grey' with any other color code
 		}
 		// Otherwise, use the color scale
 		return colorScale(product_name);
@@ -112,21 +127,24 @@
         updateVisualization(); // Update the visualization with the new value of typeselection
     }
 
+    function handleToggleChange(event) {
+    includeOther = event.target.checked;
+    updateVisualization();
+    }
+
     // Create a writable store to hold the HTML content
     const stackedBarsHTMLStore = writable('');
 
     function updateVisualization(){
-        let newstackedBarsHTML = ''; // Initialize a new variable to hold the HTML content
         // Construct SVG for the current year
-        let svgHTML = `<svg width="${width}" height="${height}" class="translated" transform="translate(${margin.left}, ${margin.top})">`;
+        let newstackedBarsHTML = `<svg width="${width}" height="${height}" class="translated" transform="translate(${margin.left}, ${margin.top})">`;
         // Re-initialize the value of total
         total = 0;
         // Loop through each year and create stacked bars
         const years = [2019, 2020, 2021, 2022, 2023];
         years.forEach(year => {
             // Filter and select products for the current year
-            const bestProducts = filterAndSelectProducts(typeselection, data, year, n);
-            console.log(bestProducts)
+            const bestProducts = filterAndSelectProducts(typeselection, data, year, n, includeOther);
 
             // Calculate total revenue for the current year
             let yeartotal = bestProducts.reduce((acc, product) => acc + product.revenue, 0);
@@ -135,8 +153,6 @@
             if (yeartotal > total) {
                 total = yeartotal;
             }
-            // Construct SVG for the current year
-            let svgHTML = `<svg width="${width}" height="${height}" class="translated" transform="translate(${margin.left}, ${margin.top})">`;
 
             // Loop through each product for the current year
             bestProducts.slice().reverse().forEach((bp, i) => {
@@ -145,8 +161,7 @@
                 let y = 0;
                 // Calculate y position based on cumulativeHeight
                 if (i == 0){
-                    y = height - rescale(bp.revenue, total, inner_height);
-                    //console.log(y)
+                    y = margin.top + inner_height - rescale(bp.revenue, total, inner_height);
                 }else{
                     let cumulativeHeight = 0;
                     const N = bestProducts.length;
@@ -154,25 +169,71 @@
                         const revenue = parseFloat(bestProducts[j].revenue);
                         cumulativeHeight += revenue;
                     }
-                y = height - rescale(cumulativeHeight + bp.revenue, total, inner_height);
-                //console.log(y)
+                y = margin.top + inner_height - rescale(cumulativeHeight + bp.revenue, total, inner_height);
                 }
 
                 // Construct rect element for the current product
-                const rectHTML = `<rect x="${x}" y="${y}" width="${bar_width}" height="${calculateHeight(bp)}" fill="${getColor(bp.product_name)}"></rect>`;
+                const rectHTML = `<rect class="bar" x="${x}" y="${y}" width="${bar_width}" height="${calculateHeight(bp)}" fill="${getColor(bp.product_name)}"
+                                data-product_name="${bp.product_name}"
+                                onmouseover="document.getElementById('tooltip').style.opacity='1'; 
+                                            document.getElementById('tooltip').innerHTML='${bp.product_name}, ${bp.revenue/1000000} MCP'; 
+                                            document.getElementById('tooltip').style.left=event.pageX + 10 + 'px'; 
+                                            document.getElementById('tooltip').style.top=event.pageY - 28 + 'px';"
+                                onmouseout="document.getElementById('tooltip').style.opacity='0';"></rect>`;
 
                 // Append rect element to SVG HTML
-                svgHTML += rectHTML;
+                newstackedBarsHTML += rectHTML;
             });
-
-        // Close SVG tag
-        svgHTML += `</svg>`;
-
-        // Add SVG HTML for the current year to stackedBarsHTML
-        newstackedBarsHTML += svgHTML;
 
         });
 
+        // Close group container for bars
+        newstackedBarsHTML += `<g class="bars"></g>`        
+
+        // Values to relocate the x axis so that the labels of the years fall exactly in the middle of the bars
+        let xlimit_left = 2019 - ((bar_width/2) /  (bar_width + bar_distance));
+        let xlimit_right = 2023 + ((bar_width/2) /  (bar_width + bar_distance));
+
+        let xScale = d3.scaleLinear()
+                        .domain([xlimit_left, xlimit_right]) // input 2019, 2020, 2021, 2022, 2023
+                        .range([0, inner_width]); // output 
+
+        let yScale = d3.scaleLinear()
+                        .domain([0, total/1000000])  // input
+                        .range([inner_height, 0]); // output 
+
+        
+        // Create a temporary SVG element for axes
+        const tempSvg = d3.select(document.createElementNS(d3.namespaces.svg, 'svg'));
+
+        // Append x-axis
+        tempSvg.append('g')
+            .attr('transform', `translate(${margin.left},${inner_height + margin.top})`)
+            .call(d3.axisBottom(xScale).tickFormat(d3.format("")) // Format ticks as integers without any decimal points
+                .ticks(5))
+            .style('font-size', '20px');
+
+        // Append y-axis
+        tempSvg.append('g')
+            .attr('transform', `translate(${margin.left},${margin.top})`)
+            .style('font-size', '20px')
+            .call(d3.axisLeft(yScale)
+                .ticks(5)) // adjust the number of ticks if needed
+            
+        
+        tempSvg.append('text')
+            .attr('x', -(inner_height / 2))
+            .attr('y', margin.left / 3)
+            .attr('transform', 'rotate(-90)')
+            .attr('text-anchor', 'middle')
+            .text('Revenues [MCP]')
+            .style('font-size', '24px'); // Adjust font size if needed
+
+        // Add axes HTML to stackedBarsHTML
+        newstackedBarsHTML += tempSvg.html();
+
+        // Close SVG tag
+        newstackedBarsHTML += `</svg>`;
 
         // Update the value of the store with the new HTML content
         stackedBarsHTMLStore.set(newstackedBarsHTML);
@@ -182,13 +243,15 @@
 
 </script>
 
-<div>
-    <label for="slider">Number of Top Elements (n):</label>
+<h1> Revenues coming from the top n products (per product type) over time </h1>
+
+<div style="font-size: 24px">
+    <label for="slider">Number of n Top Elements:</label>
     <input type="range" id="slider" min="3" max="20" step="1" bind:value={n} on:input={handleSliderInput}>
     <span>{n}</span>
 </div>
 
-<div>
+<div style="font-size: 24px">
     <label for="typeselection">Product Type Selection:</label>
     <select id="typeselection" bind:value={typeselection} on:change={handleDropdownChange}>
         <option value="ALL">All Products</option>
@@ -204,4 +267,26 @@
     </select>
 </div>
 
+<div style="font-size: 24px">
+    <label for="toggle">Include 'Other' Component:</label>
+    <input type="checkbox" id="toggle" bind:checked={includeOther} on:change={handleToggleChange}>
+</div>
+
 {@html $stackedBarsHTMLStore}
+
+<style>
+    .tooltip {
+      position: absolute;
+      text-align: center;
+      padding: 5px;
+      font-size: 16px;
+      background: rgb(240, 239, 240);
+      border: 0px solid;
+      border-radius: 5px;
+      pointer-events: none;
+      opacity: 0;
+      transition: opacity 0.2s;
+    }
+</style>
+
+<div id="tooltip" class="tooltip"></div>
